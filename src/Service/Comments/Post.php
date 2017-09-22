12c3ad840d26f1a0512ffca521beb69c05f13ab2
jasminalackovic <lackovicjasmina@gmail.com>
2017-09-22 13:37:56 +0200

    Update project

---
+++ src/Service/Comments/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service\Comments;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class Index
+{
+
+    public function run(Request $request, Response $response)
+    {
+
+    }
+}
